7e2cd5b768c09671bf51adda09f3bdbab84554c5
VitaliyVorontsov <38605008+VitaliyVorontsov@users.noreply.github.com>
2019-04-02 16:30:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 3, j = 4;
+            Console.WriteLine("Product right move:");
+            var a = Task1.RightMove(i, j);
+            Console.WriteLine(a);
+            Console.WriteLine();
+            Console.WriteLine("\n\n2 ============================================" +
+                "\n================================================\n\n");
+            (string def, long remainder, int quotient) = Task2.Devide(22, 6);
+            Console.WriteLine($"Remeinder: {remainder}\nQuotient: {quotient}\n{def}");
+
+            Console.WriteLine("\n\n3 ============================================" +
+            "\n================================================\n\n");
+
+            float a1 = 3.5f, b1 = 1f;
+            Console.WriteLine("Floating number addition:");
+            var (def3, bin, result) = Task3.Add(a1, b1);
+            Console.WriteLine(def3);
+            Console.WriteLine($"answ : {bin} = {result}");
+        }
+    }
+}
---
+++ Task3.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    public class Task3
+    {
+        public static (string def, string binAnsw, float answ) Add(float a1, float a2)
+        {
+            uint a1_1 = BitConverter.ToUInt32(BitConverter.GetBytes(a1), 0);
+            uint a2_2 = BitConverter.ToUInt32(BitConverter.GetBytes(a2), 0);
+
+            uint s1 = a1_1 >> 31;
+            uint s2 = a2_2 >> 31;
+            uint e1 = (a1_1 >> 23) & 0xFF;
+            uint e2 = (a2_2 >> 23) & 0xFF;
+            uint m1 = (a1_1 & 0x7FFFFF) + 8388608;
+            uint m2 = (a2_2 & 0x7FFFFF) + 8388608;
+
+            string def = $"{a1} + {a2}\n\n";
+            def += $"a1 = {Convert.ToString(a1_1, 2)}\n";
+            def += $"a2 = {Convert.ToString(a2_2, 2)}\n\n";
+            def += $"s1 = {s1} e1 = {Convert.ToString(e1, 2)} m1 = {Convert.ToString(m1, 2)}\n";
+            def += $"s2 = {s2} e2 = {Convert.ToString(e2, 2)} m2 = {Convert.ToString(m2, 2)}\n";
+
+            if (((e1 << 23) + m1) < ((e2 << 23) + m2))
+            {
+                uint temp = a1_1;
+                a1_1 = a2_2;
+                a2_2 = temp;
+                temp = e1;
+                e1 = e2;
+                e2 = temp;
+                temp = s1;
+                s1 = s2;
+                s2 = temp;
+                temp = m1;
+                m1 = m2;
+                m2 = temp;
+
+                def += "\nswap the values, because Abs(a1) < Abs(a2)\n";
+                def += $"s1 = {s1} e1 = {Convert.ToString(e1, 2)} m1 = {Convert.ToString(m1, 2)}\n";
+                def += $"s2 = {s2} e2 = {Convert.ToString(e2, 2)} m2 = {Convert.ToString(m2, 2)}\n";
+            }
+
+            uint e3 = e1;
+
+            m2 >>= (int)(e1 - e2);
+            def += $"\nRight shift m2 by the exponent difference {e1 - e2}\n";
+            def += $"s2 = {s2} e2 = {Convert.ToString(e2, 2)} m2 = {Convert.ToString(m2, 2)}\n";
+
+            uint m;
+
+            if (s1 == s2)
+                m = m1 + m2;
+            else
+                m = m1 - m2;
+
+            def += $"\nCompute the {(s1 == s2 ? "sum" : "difference")} of the mantissas\n";
+            def += $"s3 = {s1} e3 = {Convert.ToString(e3, 2)} m3 = {Convert.ToString(m, 2)}\n";
+
+
+            if (m >> 23 != 1)
+            {
+                int len;
+                uint tmp = m;
+                for (len = 0; tmp != 0; tmp >>= 1, len++) ;
+
+                e3 += (uint)(len - 24);
+                if (len > 24)
+                    m >>= (len - 24);
+                else
+                    m <<= (24 - len);
+
+                def += "\nNormalize the resultant\n";
+                def += $"s3 = {s1} e3 = {Convert.ToString(e3, 2)} m3 = {Convert.ToString(m, 2)}\n";
+            }
+
+            uint answ = (((s1 << 8) + e3) << 23) + (m & 0x7FFFFF);
+            float f_answ = BitConverter.ToSingle(BitConverter.GetBytes(answ), 0);
+
+            return (def, Convert.ToString(answ, 2), f_answ);
+        }
+    }
+}
